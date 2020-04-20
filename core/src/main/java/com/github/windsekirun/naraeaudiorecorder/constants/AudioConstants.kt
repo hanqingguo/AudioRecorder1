f5dc910b4b0068675fa728fedcd2d221a2f0d28c
hanqing <18523978636@163.com>
2020-04-20 15:09:36 -0400

    add

---
+++ core/src/main/java/com/github/windsekirun/naraeaudiorecorder/constants/AudioConstants.kt
@@ -0,0 +1,11 @@
+package com.github.windsekirun.naraeaudiorecorder.constants
+
+/**
+ * Constants for recording audio
+ */
+object AudioConstants {
+    const val FREQUENCY_44100 = 44100
+    const val FREQUENCY_192000 = 192000
+    const val REFERENCE_MAX_AMP = 0.6
+    const val SILENCE_THRESHOLD = 2700
+}
